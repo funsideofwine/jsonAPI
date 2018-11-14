bced5c16ca683483d912cc2f656fa3a171b4b18f
Rolly Falco Villacacan <32959267+funsideofwine@users.noreply.github.com>
2018-11-14 08:31:24 +0300

    this one works for POST and GET method

--- 1.php
+++ 1.php
@@ -1,31 +1,31 @@
+<?php 
 
+
+
+?>
 
 <!DOCTYPE html>
 <html>
 <body>
 
-<h2>Create JSON string from a JavaScript array using POST.</h2>
+<h2>Create JSON string from a JavaScript array.</h2>
 
 <p id="demo"></p>
 
 <script>
 
-obj = { "table":"tbl_members", "first_name":"rol" };
-//obj = { "table":"customers", "limit":10 };
+obj = { "table":"tbl_members", "first_name":"i" };
 dbParam = JSON.stringify(obj);
 xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        myObj = JSON.parse(this.responseText);
-        for (x in myObj) {
-            txt += myObj[x].first_name + "<br>";
-        }
-        document.getElementById("demo").innerHTML = txt;
+        document.getElementById("demo").innerHTML = this.responseText;
     }
 };
-xmlhttp.open("POST", "api_post.php", true);
-xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xmlhttp.send("x=" + dbParam);
+//xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xmlhttp.open("POST", "2.php?x=" + dbParam, true);
+//xmlhttp.open("GET", "2.php?x=" + dbParam, true);
+xmlhttp.send();
 
 </script>
 
